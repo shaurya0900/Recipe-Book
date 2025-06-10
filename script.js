115a686fdd03409c49b27c5c1730aac98aecebae
shaurya0900 <shaurya2929@gmail.com>
2025-06-10 23:17:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "275d58779ccf4e22af03e792e8819fff";
+const API_KEY = "";
 const recipeListEl = document.getElementById("recipe-list");
 
 const refresherSelector=document.querySelector('.refreshButton')
